35015c452e0cc143880c00afc75b1a11c6f6699f
Ashish Prajapati <62009244+Ashish-simpleCoder@users.noreply.github.com>
2025-01-07 12:40:25 +0000

    feat: add binary tree insertion

---
+++ src/tree/index.test.ts
@@ -0,0 +1,114 @@
+import { expect, it } from "vitest"
+
+it("should create binary-tree", () => {
+    const tree = new Tree()
+
+    tree.add(10)
+    tree.add(20)
+    tree.add(30)
+
+    tree.add(40)
+    tree.add(50)
+
+    tree.add(60)
+    tree.add(70)
+
+    console.log(tree.inorderTravese())
+
+
+    const root = tree.root!
+    expect(root.data).toBe(10)
+    expect(root.left!.data).toBe(20)
+    expect(root.right!.data).toBe(30)
+
+    expect(root.left!.left!.data).toBe(40)
+    expect(root.left!.right!.data).toBe(50)
+
+    expect(root.right!.left!.data).toBe(60)
+    expect(root.right!.right!.data).toBe(70)
+})
+
+
+export class Node {
+    data: number
+    left: Node | null = null
+    right: Node | null = null
+
+    constructor(data) {
+        this.data = data
+    }
+}
+
+export class Tree {
+    root: null | Node = null
+    constrcutor() {
+
+    }
+
+    // inorder traversal
+    // left -> root -> right
+    inorderTravese() {
+        const result: number[] = []
+
+        traverse(this.root)
+
+        function traverse(node: Node | null) {
+            if (!node) return
+            traverse(node.left)
+            result.push(node.data)
+            traverse(node.right)
+        }
+
+        return result
+    }
+
+
+    // The idea is to do an iterative level order traversal of the given tree using queue. 
+    // If we find a node whose left child is empty, 
+    // we make a new key as the left child of the node. 
+    // Else if we find a node whose right child is empty, 
+    // we make the new key as the right child. 
+    // We keep traversing the tree until 
+    // we find a node whose either left or right child is empty 
+    add(data) {
+        const newNode = new Node(data)
+
+        if (this.root == null) {
+            this.root = newNode
+        } else {
+            addNode(this.root, newNode)
+        }
+
+        function addNode(parentNode: Node, newNode: Node) {
+            const root = parentNode
+
+            let queue: Node[] = [];
+            queue.push(root);
+
+            while (queue.length > 0) {
+
+                let curr = queue.shift();
+                if (!curr) return
+
+                // First check left if left is null 
+                // insert node in left otherwise check for right
+                if (curr.left !== null)
+                    queue.push(curr.left);
+                else {
+                    curr.left = newNode;
+                    break;
+                }
+
+                if (curr.right !== null)
+                    queue.push(curr.right);
+                else {
+                    curr.right = newNode;
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+}
